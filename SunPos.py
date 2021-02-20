8d27c518129d4376946e6465f62d7bcb812b8f9b
Simone jovenitti <jovenitti@users.noreply.github.com>
2021-02-20 22:34:38 +0100

    grafici con immagini png

--- SunPos.py
+++ SunPos.py
@@ -1,10 +1,12 @@
 #-- COSTANTI
 ASSE = 23.45 
+DPI = 72
 
 import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as image
 from pathlib import Path
 import astropy.units as u
 from astropy.time import Time
@@ -16,7 +18,7 @@ from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 
 
 
-num  = 3    # scegli la location (tra 0:20)
+num  = 4    # scegli la location (tra 0:20)
 
 
 
@@ -79,7 +81,13 @@ plt.savefig(folder+'_culminazione.png')
 plt.show()
 
 
-sys.exit(0)
+
+
+
+
+#sys.exit(0)
+
+
 
 
 
@@ -87,17 +95,50 @@ sys.exit(0)
 #     VIDEO      #
 #----------------#
 
+
+#-- SHRINK ORIGINAL IMAGE
+from PIL import Image
+size = 100, 100
+im = Image.open('sun.png')
+im.thumbnail(size, Image.ANTIALIAS)
+im.save('isun.png')
+
+
+#-- PLOT COL SOLE
+im = image.imread('isun.png')
+image_size = im.shape[1], im.shape[0]
+
+
+#-- FACCIO LE IMMAGINI
 for i in range(len(de_t)):
-    fig=plt.figure()
-    plt.plot(az_arr[i], alt_arr[i], marker="o", linewidth=0.5, linestyle='dashed', markersize=12)
-    plt.xlim(110+bussola, 250+bussola)
-    plt.ylim(40, 80)
-    plt.savefig(folder+'img'+str(i)+'.png', dpi=72, bbox_inches='tight')#, pad_inches=10.)
+    fig = plt.figure(dpi=DPI, figsize=(12, 8))
+    ax  = fig.add_subplot(111)
+    line, = ax.plot(az_arr[i], alt_arr[i], "bo",mfc="None",mec="None",markersize=image_size[0] * (DPI/ 96))
+    ax.patch.set_alpha(0)
+    ax.set_xlim((110+bussola, 250+bussola))
+    ax.set_ylim((40, 80))
+    line._transform_path()
+    path, affine = line._transformed_path.get_transformed_points_and_affine()
+    path = affine.transform_path(path)
+    for pixelPoint in path.vertices: fig.figimage(im, pixelPoint[0]-image_size[0]/1.5, pixelPoint[1]-image_size[1]/1.5, origin="lower")
+    plt.savefig(folder+'img'+str(i)+'.png', dpi=DPI, bbox_inches='tight')#, pad_inches=10.)
     plt.close(fig)
     print(i)
-    
-istruzione = "ffmpeg -r 1 -i "+folder+"img%01d.png -vcodec mpeg4 -y "+video
-os.system("ffmpeg -r 1 -i loc_03/img%01d.png -vcodec mpeg4 -y "+video)
+
+
+#-- PLOT SENZA MARKER SOLE
+#for i in range(len(de_t)):
+#    fig=plt.figure()
+#    plt.plot(az_arr[i], alt_arr[i], marker="o", linewidth=0.5, linestyle='dashed', markersize=12)
+#    plt.xlim(110+bussola, 250+bussola)
+#    plt.ylim(40, 80)
+#    plt.savefig(folder+'img'+str(i)+'.png', dpi=DPI, bbox_inches='tight')#, pad_inches=10.)
+#    plt.close(fig)
+#    print(i)
+
+
+#-- FACCIO IL VIDEO
+os.system("ffmpeg -r 1 -i "+folder+"img%01d.png -vcodec mpeg4 -y "+video)
 os.system("mv "+video+" " + folder)
 
 
